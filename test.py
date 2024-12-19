4266d154561672b78772e3637c91a6514ba6f52e
soroush-bn <soroush.baghernezhad3@gmail.com>
2024-12-19 12:55:07 -0330

    testing cluster

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import yaml
 import os 
+import shutil
 
-
-with open("config.yaml", "r") as file:
-    config = yaml.safe_load(file)
+# with open("config.yaml", "r") as file:
+#     config = yaml.safe_load(file)
+# # directory = config["saving_dir"] +'/'+ config["first_name"]+'_' + config['last_name']+'_' + config["experiment_name"] 
 # directory = config["saving_dir"] +'/'+ config["first_name"]+'_' + config['last_name']+'_' + config["experiment_name"] 
-directory = config["saving_dir"] +'/'+ config["first_name"]+'_' + config['last_name']+'_' + config["experiment_name"] 
-print
-# directory = os.path.dirname()
-if directory and not os.path.exists(directory):
-    os.makedirs(directory)
+# # directory = os.path.dirname()
+# if directory and not os.path.exists(directory):
+#     os.makedirs(directory)
 
 
-df = pd.read_csv(r"E:\projects\noraxon\noraxon-sensor\data\aliso_Baghernezhad_second\final_df.csv")
-print(df.head())
+# df = pd.read_csv(r"E:\projects\noraxon\noraxon-sensor\data\aliso_Baghernezhad_second\final_df.csv")
+# print(df.head())
+
+print("this should be a test for the cluster.")
